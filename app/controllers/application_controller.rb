9017e4a846e3302755ebe7dbe3c50ca9aa51b919
NGENZI Herve <48619051+muhenge@users.noreply.github.com>
2021-10-31 18:07:36 +0200

    JWT token session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,11 @@ class ApplicationController < ActionController::API
       render json: resource.errors, status: 400
     end
   end
+
+def encode_token(payload = {})
+  exp = 24.hours.from_now
+  payload[:exp] = exp.to_i
+  JWT.encode(payload, Rails.application.secrets.secret_key_base, 'HS256')
+end
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,17 @@ class SessionsController < Devise::SessionsController
   private
 
   def respond_with(resource, _opts = {})
-    render_jsonapi_response(resource)
+    render json: { 
+      user: resource,
+      message: 'You are logged in.' ,
+      token: encode_token({
+        resource: resource
+      })
+      }, status: :ok
+  #   render json: {jwt: encode_token({
+  #     email: @user.email,
+  # })}
+
   end
 
   def respond_to_on_destroy
